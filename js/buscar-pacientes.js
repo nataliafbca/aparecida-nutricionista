178e3b31f01cde8e3f279428ff111651f2d62da4
NATALIA <natliaacastroo@gmail.com>
2020-08-04 11:06:09 -0300

    first commit

---
+++ js/buscar-pacientes.js
@@ -0,0 +1,29 @@
+var botaoAdicionar = document.querySelector("#buscar-pacientes");
+
+botaoAdicionar.addEventListener("click", function(){
+    console.log("Buscando pacientes...");
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+
+    xhr.addEventListener("load", function(){
+        if( xhr.status == 200 ) {
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
+
+            pacientes.forEach( function(paciente) {
+                adicionaPacienteNaTabela(paciente);
+            });
+        } else {
+            console.log(xrh.status);
+            console.log(xrh.responseText);
+            var erroAjax = document.querySelector("#erro-ajax");
+            erroAjax.classList.remove("invisivel");
+        }
+
+    });
+
+    xhr.send();
+    
+});
